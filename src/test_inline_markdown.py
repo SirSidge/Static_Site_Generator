847b0ecce8e8c43a718a2de222efbb39b2f8a2c0
SirSidge <heinz.hscharlach@gmail.com>
2025-05-05 18:48:50 +0200

    Added functions to find images and links

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 class TestSplitNodesByDelimiter(unittest.TestCase):
@@ -36,4 +36,27 @@ class TestSplitNodesByDelimiter(unittest.TestCase):
                 TextNode("another", TextType.BOLD),
             ],
             new_nodes,
+        )
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_find(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(
+            extract_markdown_images(text), 
+            [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+            )
+        
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_find(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(
+            extract_markdown_links(text), 
+            [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+            )
+    
+    def test_link_with_image(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)"
+        self.assertEqual(
+            extract_markdown_links(text), 
+            [("to boot dev", "https://www.boot.dev")]
         )
