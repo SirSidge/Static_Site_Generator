7b41f8b4faa73b736bfaad96f431b17dbc4f702a
Heinz <heinz.hscharlach@gmail.com>
2025-05-29 21:18:26 +0200

    Started recursion practice

---
+++ src/recursion_practice.py
@@ -0,0 +1,23 @@
+"""def factorial(n):
+    if n <= 1:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5))"""
+
+"""def sum_natural(n):
+    if n <= 1:
+        return 1
+    return n + sum_natural(n - 1)
+
+print(sum_natural(5))"""
+
+nr1 = 0
+nr2 = 1
+def fibonacci(n):
+    if n <= 0:
+        return 1 + 0
+    
+
+
+print(fibonacci(6))
