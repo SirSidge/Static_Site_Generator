6a6e7dba285e4a6d5165df1006552aa312f3ec16
Heinz <heinz.hscharlach@gmail.com>
2025-06-02 18:25:33 +0200

    Updated Static directories

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 import os, shutil
 
-from textnode import TextNode, TextType
-
 def main():
     shutil.rmtree("public", True)
     os.mkdir("public")
-    print(os.listdir("static"))
+    static_directories(os.listdir("static"))
+    shutil.copy("static/index.css", "public")
+    
 
 def static_directories(static_dir):
-    pass
+    for i in range(len(static_dir)):
+        if os.path.isfile(os.path.join("static", {static_dir[i]})):
+            print(f"Yes {static_dir[i]} is a file")
+        else:
+            print(f"No {static_dir[i]} is a directory")
+    return static_dir
 
 main()
