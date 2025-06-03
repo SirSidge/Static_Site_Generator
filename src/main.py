c4f52205faea6d634624a8aec2de0aedab3ecd9a
Heinz <heinz.hscharlach@gmail.com>
2025-06-03 17:21:59 +0200

    Updated static directories

--- src/main.py
+++ src/main.py
@@ -3,16 +3,15 @@ import os, shutil
 def main():
     shutil.rmtree("public", True)
     os.mkdir("public")
-    static_directories(os.listdir("static"))
-    shutil.copy("static/index.css", "public")
+    static_directories("static")
     
-
 def static_directories(static_dir):
-    for i in range(len(static_dir)):
-        if os.path.isfile(os.path.join("static", {static_dir[i]})):
-            print(f"Yes {static_dir[i]} is a file")
-        else:
-            print(f"No {static_dir[i]} is a directory")
-    return static_dir
+    if os.path.isfile(static_dir):
+        return static_dir
+    next_level = os.listdir(static_dir)
+    for i in range(len(next_level)):
+        new_path = static_directories(os.path.join(static_dir, next_level[i]))
+        if type(new_path) is not type(None):
+            shutil.copy(new_path, "public")
 
 main()
