0c5188ecf86b9084d8bb092af169206b4b95b632
Heinz <heinz.hscharlach@gmail.com>
2025-06-04 22:14:53 +0200

    Updated static directory again. I'm close

--- src/main.py
+++ src/main.py
@@ -9,9 +9,13 @@ def static_directories(static_dir):
     if os.path.isfile(static_dir):
         return static_dir
     next_level = os.listdir(static_dir)
+    new_dir = "public"
     for i in range(len(next_level)):
+        if not os.path.isfile(os.path.join(static_dir, next_level[i])):
+            new_dir = os.path.join(new_dir, next_level[i])
+            os.mkdir(new_dir)
         new_path = static_directories(os.path.join(static_dir, next_level[i]))
         if type(new_path) is not type(None):
-            shutil.copy(new_path, "public")
+            shutil.copy(new_path, new_dir)
 
 main()
